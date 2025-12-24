f274f97dd331bf44c2892fe93d95a1a250feb9b0
Vipin Girjapure <vipin.girjapure@gmail.com>
2025-12-24 14:20:54 +0530

    [VG] started with admin login UI

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "../App.css";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import bgImage from "../assets/AuthBackgroundImg.jpg";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
+  return (
+    <div
+      className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat relative px-4 sm:px-6 lg:px-8"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="w-full max-w-[500px] bg-white rounded-[10px] shadow-2xl p-8 sm:p-10 md:p-12 relative z-10 mx-auto">
+        <div className="text-center mb-10">
+          <h1 className="md:text-2xl text-xl text-(--primary-color) sm:text-3xl font-medium mb-2">
+            Sign In
+          </h1>
+          <p className="text-(--text-color-gray) text-sm sm:text-base">
+            Let's build something great
+          </p>
+        </div>
+
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <Input
+            id="email"
+            label="E-mail or phone number"
+            type="text"
+            placeholder="Enter your email or phone"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <div className="space-y-1">
+            <Input
+              id="password"
+              label="Password"
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <Button type="submit">Login</Button>
+
+          <div className="flex justify-end pt-2">
+            <a
+              href="#"
+              className="text-sm font-normal text-(--primary-color) hover:opacity-80 transition-colors"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
