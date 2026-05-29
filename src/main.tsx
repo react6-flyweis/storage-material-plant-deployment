7848a8227f3030259a62e5dbb9425ec25e2c8d56
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-05-29 23:24:32 +0530

    feat: integrate PersistGate with LoadingScreen for improved loading handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
 import "./index.css";
 import App from "./App.tsx";
+import LoadingScreen from "./components/LoadingScreen";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
