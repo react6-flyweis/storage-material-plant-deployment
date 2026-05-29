e7fd02fd8d3543ad0a933e3fcfd9e6c937862092
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-05-29 23:25:15 +0530

    feat: implement ApiResponse interface and enhance auth reducer with persistence

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,46 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { authApi } from "./api/authApi";
+import { logisticsApi } from "./api/logisticsApi";
+import { projectApi } from "./api/projectApi";
 import authReducer from "./slices/authSlice";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["user", "accessToken", "refreshToken", "role", "isAuthenticated"],
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [logisticsApi.reducerPath]: logisticsApi.reducer,
+    [projectApi.reducerPath]: projectApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/PAUSE",
+          "persist/PURGE",
+          "persist/FLUSH",
+          "persist/REGISTER",
+        ],
+      },
+    }).concat(
+      authApi.middleware,
+      logisticsApi.middleware,
+      projectApi.middleware,
+    ),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
