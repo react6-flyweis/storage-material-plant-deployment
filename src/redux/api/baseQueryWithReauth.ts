c25afbc751bd60fd42ddee27c34d49a05ab89c7b
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-06-03 16:04:22 +0530

    refactor: update base API URL to use environment variable and fix type formatting in baseQueryWithReauth

--- src/redux/api/baseQueryWithReauth.ts
+++ src/redux/api/baseQueryWithReauth.ts
@@ -8,7 +8,7 @@ import type { ApiResponse } from "./apiResponse";
 import type { RootState } from "../store";
 import { logout, updateTokens } from "../slices/authSlice";
 
-const apiBaseUrl = "http://13.201.52.212";
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "";
 
 const rawBaseQuery = fetchBaseQuery({
   baseUrl: apiBaseUrl,
@@ -74,8 +74,8 @@ export const baseQueryWithReauth: BaseQueryFn<
 
   const refreshData = refreshResult.data as
     | ApiResponse<{
-      accessToken: string;
-    }>
+        accessToken: string;
+      }>
     | undefined;
   const nextAccessToken = refreshData?.data?.accessToken;
 
