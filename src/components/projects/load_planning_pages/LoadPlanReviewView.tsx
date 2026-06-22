9f5d6abff1ba89b95ee82b2fce7f47c73f9402ef
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-06-22 19:57:11 +0530

    style(LoadPlanReviewView): adjust mobile padding

--- src/components/projects/load_planning_pages/LoadPlanReviewView.tsx
+++ src/components/projects/load_planning_pages/LoadPlanReviewView.tsx
@@ -239,7 +239,7 @@ const LoadPlanReviewView: React.FC = () => {
         description="Final check of the entire load plan, including bundles, trucks, and weights, before selecting freight carriers."
         actions={actions}
       />
-      <div className="p-6 pt-0">
+      <div className="md:p-6 p-3 pt-0">
         <Step6LoadPlanReview
           onViewPackingList={(row) => {
             setSelectedPackingList(row);
